953ed1a66a4fb258d55b20c2b78798944cc08a33
julioongebele <127917755+julioongebele@users.noreply.github.com>
2023-04-08 15:19:01 +0700

    Add files via upload

---
+++ liga 1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pemograman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi taxi = new taxi();
+
+            taxi.DriverName = "marto";
+            taxi.OnDuty = "true";
+            taxi.NumberOfPassenger = "10";
+
+            taxi.taxInfo();
+            taxi.PickupPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
---
+++ pemogranam.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pemograman
+{
+    internal class taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public string OnDuty { get; set; }
+        public string NumberOfPassenger { get; set; }
+
+
+        //method
+        public void taxInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0} ", OnDuty);
+            Console.WriteLine("Number Of Passenger: ", NumberOfPassenger);
+        }
+
+        public void PickupPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+
+    }
+}
